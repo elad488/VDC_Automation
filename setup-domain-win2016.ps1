736af5cf985f7bf094bc79d4c2d22fc808e998b5
Elad Hascal <elhascal@microsoft.com>
2018-10-29 15:29:01 +0200

    Update setup-domain-win2016.ps1

---
+++ setup-domain-win2016.ps1
@@ -0,0 +1,21 @@
+﻿install-windowsfeature RSAT-ADDS,AD-Domain-Services
+
+Import-Module ADDSDeployment
+
+$secpasswd = ConvertTo-SecureString "p@ssword1!" -AsPlainText -Force
+$mycreds = New-Object System.Management.Automation.PSCredential ("vmadmin", $secpasswd)
+
+
+Install-ADDSForest `
+-CreateDnsDelegation:$false `
+-DatabasePath “C:\Windows\NTDS” `
+-DomainMode “Win2012R2” `
+-DomainName “testdom.com” `
+-DomainNetbiosName “TESTDOM” `
+-ForestMode “Win2012R2” `
+-InstallDns:$true `
+-LogPath “C:\Windows\NTDS” `
+-NoRebootOnCompletion:$false `
+-SysvolPath “C:\Windows\SYSVOL” `
+-Force:$true `
+-SafeModeAdministratorPassword $secpasswd
